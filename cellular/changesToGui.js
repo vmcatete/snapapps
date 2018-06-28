31594ee234e3803a374fcdda22dd8f48752bd538
stemc <stemc@stemc.csc.ncsu.edu>
2018-06-28 13:46:26 -0400

    don't load project in view mode.

--- cellular/changesToGui.js
+++ cellular/changesToGui.js
@@ -622,6 +622,7 @@ IDE_Morph.prototype.rawSaveProject = function (name, table) {
 IDE_Morph.prototype.loadExampleProject = function(name) {
     Trace.log("IDE.loadExampleProject", name);
     if (!name) return;
+    if (name === "Viewing") return;
     var examples = ide.getMediaList("Examples");
     var resourceURL = null;
     examples.forEach(function(example) {
@@ -795,7 +796,7 @@ IDE_Morph.prototype.cloudMenu = function() {
         menu.addItem(
             'open shared project from cloud...',
             function () {
-                myself.prompt('Author nameâ€?', function (usr) {
+                myself.prompt('Author nameï¿½?', function (usr) {
                     myself.prompt('Project name...', function (prj) {
                         var id = 'Username=' +
                             encodeURIComponent(usr.toLowerCase()) +
