35654c28791423374b8eb02ab4e6643e033b2589
ydong2 <ydong2@ncsu.edu>
2019-05-04 10:42:43 -0400

    Added option for hashing userID and allowing new user

--- logging/config.example.php
+++ logging/config.example.php
@@ -10,4 +10,10 @@ $table = "trace";
 // will all be lost. It must be 22+ characters
 $salt = "random static salt of at least 22 characters";
 
+// If true, user IDs will be hashed before saving to the database
+$hashUserID = false;
+
+// Choose a user table, could be the same as the "trace" table if allow new user
+// $user_table = "trace";
+$user_table = "user_table_name";
 ?>
