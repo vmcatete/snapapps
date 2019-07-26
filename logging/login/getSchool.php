66bf720de6b606c7225f5824f70b096578a8464c
Yihuan Dong <ydong2@ncsu.edu>
2019-07-26 13:49:25 -0400

    Removed select school since it's no longer needed

---
+++ logging/login/getSchool.php
@@ -0,0 +1,29 @@
+<?php
+include('../config.php');
+    // single user login
+    if (!array_key_exists('id', $_POST)) {
+        return;
+    }
+    $id = strtolower($_POST['id']);
+
+    $new = array_key_exists('new', $_POST) && $_POST['new'] === 'true';
+    if (!$new) {
+        $mysqli = new mysqli($host, $user, $password, $db);
+        if ($mysqli->connect_errno) {
+            http_response_code(503);
+            die ("Failed to connect to MySQL: ($mysqli->errno) $mysqli->error");
+        }
+        $query = "SELECT 1 FROM $user_table WHERE userID='$hash' LIMIT 1";
+        $result = $mysqli->query($query);
+        if (!$result) {
+            http_response_code(500);
+            die ("Failed to retrieve data: ($mysqli->errno) $mysqli->error");
+        }
+        if ($result->num_rows === 0) {
+            echo '{"newUser": true}';
+            return;
+        }
+    }
+
+    echo '{"userID": "' . $hash . '"}';
+?>
