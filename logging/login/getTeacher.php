7ecca7660794f3abb24d312f570bc016e4060db9
ydong2 <ydong2@ncsu.edu>
2019-07-27 11:52:50 -0400

    able to load teachers

--- logging/login/getTeacher.php
+++ logging/login/getTeacher.php
@@ -3,23 +3,27 @@ include('../config.php');
     if (!array_key_exists('user_name', $_POST)) {
         return;
     }
-    $id = $_POST['user_name'];
+    $user_name = $_POST['user_name'];
 
     $mysqli = new mysqli($host, $user, $password, $db);
     if ($mysqli->connect_errno) {
         http_response_code(503);
         die ("Failed to connect to MySQL: ($mysqli->errno) $mysqli->error");
     }
-    $query = "SELECT 1 FROM $user_table WHERE userID='$hash' LIMIT 1";
+    $query = "SELECT user_id, display_name FROM user where user_id IN (";
+    $query.= "SELECT teacher_id FROM session LEFT JOIN user ON session.student_id = user.user_id WHERE user.user_name = '$user_name'";
+    $query.= ")";
+
     $result = $mysqli->query($query);
     if (!$result) {
         http_response_code(500);
         die ("Failed to retrieve data: ($mysqli->errno) $mysqli->error");
     }
-    if ($result->num_rows === 0) {
-        echo '{"newUser": true}';
-        return;
+    $list = array();
+    while($row = mysqli_fetch_array($result)) {
+        $teacher->user_id = $row['user_id'];
+        $teacher->display_name = $row['display_name'];
+        array_push($list, $teacher);
     }
-
-    echo '{"userID": "' . $hash . '"}';
+    echo json_encode($list);
 ?>
