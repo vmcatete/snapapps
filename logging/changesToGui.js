5f9fe4305d29972b8a5c41ce60019bf166fa8991
Yihuan Dong <yihuandong@BARNES-5.local>
2019-07-30 17:11:14 -0400

    Removed change password and changed logout behaviour

--- logging/changesToGui.js
+++ logging/changesToGui.js
@@ -39,3 +39,150 @@ IDE_Morph.prototype.loadAssignment = function() {
     ide.controlBar.updateLabel();
     ide.controlBar.fixLayout();
 }
+
+IDE_Morph.prototype.cloudMenu = function () {
+    Trace.log("IDE.cloudMenu");
+    var menu,
+        myself = this,
+        world = this.world(),
+        pos = this.controlBar.cloudButton.bottomLeft(),
+        shiftClicked = (world.currentKey === 16);
+
+    if (location.protocol === 'file:' && !shiftClicked) {
+        this.showMessage('cloud unavailable without a web server.');
+        return;
+    }
+
+    menu = new MenuMorph(this);
+    if (shiftClicked) {
+        menu.addItem(
+            'url...',
+            'setCloudURL',
+            null,
+            new Color(100, 0, 0)
+        );
+        menu.addLine();
+    }
+    if (!this.cloud.username) {
+        menu.addItem(
+            'Login...',
+            'initializeCloud'
+        );
+        menu.addItem(
+            'Signup...',
+            'createCloudAccount'
+        );
+        menu.addItem(
+            'Reset Password...',
+            'resetCloudPassword'
+        );
+        menu.addItem(
+            'Resend Verification Email...',
+            'resendVerification'
+        );
+    } else {
+        menu.addItem(
+            localize('Logout') + ' ' + this.cloud.username,
+            'logout'
+        );
+        // menu.addItem(
+        //     'Change Password...',
+        //     'changeCloudPassword'
+        // );
+    }
+    if (shiftClicked) {
+        menu.addLine();
+        menu.addItem(
+            'export project media only...',
+            function () {
+                if (myself.projectName) {
+                    myself.exportProjectMedia(myself.projectName);
+                } else {
+                    myself.prompt('Export Project As...', function (name) {
+                        myself.exportProjectMedia(name);
+                    }, null, 'exportProject');
+                }
+            },
+            null,
+            this.hasChangedMedia ? new Color(100, 0, 0) : new Color(0, 100, 0)
+        );
+        menu.addItem(
+            'export project without media...',
+            function () {
+                if (myself.projectName) {
+                    myself.exportProjectNoMedia(myself.projectName);
+                } else {
+                    myself.prompt('Export Project As...', function (name) {
+                        myself.exportProjectNoMedia(name);
+                    }, null, 'exportProject');
+                }
+            },
+            null,
+            new Color(100, 0, 0)
+        );
+        menu.addItem(
+            'export project as cloud data...',
+            function () {
+                if (myself.projectName) {
+                    myself.exportProjectAsCloudData(myself.projectName);
+                } else {
+                    myself.prompt('Export Project As...', function (name) {
+                        myself.exportProjectAsCloudData(name);
+                    }, null, 'exportProject');
+                }
+            },
+            null,
+            new Color(100, 0, 0)
+        );
+        menu.addLine();
+        menu.addItem(
+            'open shared project from cloud...',
+            function () {
+                myself.prompt('Author name�?', function (usr) {
+                    myself.prompt('Project name...', function (prj) {
+                        myself.showMessage(
+                            'Fetching project\nfrom the cloud...'
+                        );
+                        myself.cloud.getPublicProject(
+                            prj,
+                            usr.toLowerCase(),
+                            function (projectData) {
+                                var msg;
+                                if (!Process.prototype.isCatchingErrors) {
+                                    window.open(
+                                        'data:text/xml,' + projectData
+                                    );
+                                }
+                                myself.nextSteps([
+                                    function () {
+                                        msg = myself.showMessage(
+                                            'Opening project...'
+                                        );
+                                    },
+                                    function () {nop(); }, // yield (Chrome)
+                                    function () {
+                                        myself.rawOpenCloudDataString(
+                                            projectData
+                                        );
+                                    },
+                                    function () {
+                                        msg.destroy();
+                                    }
+                                ]);
+                            },
+                            myself.cloudError()
+                        );
+
+                    }, null, 'project');
+                }, null, 'project');
+            },
+            null,
+            new Color(100, 0, 0)
+        );
+    }
+    menu.popup(world, pos);
+};
+
+IDE_Morph.prototype.logout = function () {
+    window.location.href = "../login.html";
+}
